54ea6c9f835a24fea4bc044a87d26b784fc41802
Manuel Aristarán <jazzido@jazzido.com>
2019-10-15 19:32:05 -0300

    intentando arreglar los diffs de fecha en SSR

--- components/EventCard.js
+++ components/EventCard.js
@@ -70,7 +70,10 @@ const EventCard = ({ event }) => {
   const currentDate = moment()
     .tz(config.timeZone)
     .startOf("day");
-  const eventDate = moment(event.fecha);
+  const eventDate = moment(event.fecha)
+    .tz(config.timeZone)
+    .startOf("day");
+
   const daysDiff = eventDate.diff(currentDate, "days");
   const ribbonClass = RIBBON_CLASSES[daysDiff] || "despues";
   const ribbonContent = daysDiff >= 2 ? `${daysDiff} días` : null;
