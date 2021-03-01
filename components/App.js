40d8ade484997dc71e7e7f7d46389e27ed7ba418
Manuel Aristarán <jazzido@jazzido.com>
2021-02-28 21:54:00 -0300

    fb pixel

--- components/App.js
+++ components/App.js
@@ -7,6 +7,23 @@ import "moment/locale/es";
 
 import NotificationPrompt from "./NotificationPrompt";
 
+const FB_PIXEL = `
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1325330401172361'); 
+fbq('track', 'PageView');
+</script>
+<!-- End Facebook Pixel Code -->
+`;
+
 const App = ({ children }) => (
   <main>
     <Head>
@@ -62,6 +79,7 @@ const App = ({ children }) => (
         sizes="512x512"
         href="/static/icons/icon-512x512.png"
       />
+      <script dangerouslySetInnerHTML={{__html: FB_PIXEL }} />
     </Head>
 
     <NotificationPrompt />
